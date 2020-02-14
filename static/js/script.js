7dcd7cffe5ed889a579076aff89798a3f31d452f
staceybonareri <stacylorrain@gmail.com>
2020-02-14 17:57:46 +0300

    version one

---
+++ static/js/script.js
@@ -0,0 +1,10 @@
+
+function getAkanName(){
+var gender = document.getElementById("gender").checked;
+//alert(gender);
+    if(gender == true){
+        alert("male");
+    }else if(gender == false){
+        alert("female");
+    }
+}
